eac8bd583ffdb31bfa484b0857d2ba49b80e6f8f
LaurentRDC <laurent.decotret@outlook.com>
2016-10-20 13:11:01 -0400

    ProcessReactor: Reactor running in a separate process

--- shampoo/gui/reactor.py
+++ shampoo/gui/reactor.py
@@ -1,4 +1,6 @@
 
+from multiprocessing import Process, Pipe
+from multiprocessing import Queue as ProcessSafeQueue
 import numpy as np
 from threading import Thread
 from time import sleep
@@ -109,7 +111,10 @@ class Reactor(object):
 
     def is_alive(self):
         """ Returns True if the event loop is running. Otherwise, returns False. """
-        return self.worker.is_alive()
+        try:
+            return self.worker.is_alive()
+        except: # Worker might not have been created yet
+            return False
     
     def send_item(self, item):
         """ Adds an item to the input queue. """
@@ -133,3 +138,63 @@ class Reactor(object):
         
         # Prepare for next time start() is called
         self._keep_running = True
+
+class ProcessReactor(Reactor):
+    """
+    Reactor running an event loop in a separate process.
+    """
+
+    def __init__(self, input_queue = None, output_queue = None, function = None, callback = None, **kwargs):
+        """
+        Parameters
+        ----------
+        input_queue: Queue instance or None, optional
+            Process-safe Queue object. If None (default), a local Queue is created. In this case, items can be sent to
+            the reactor using the send_item() method.
+        output_queue : Queue instance or None, optional
+            Process-safe Queue object, or process-safe Queue object. If None (default), processed items are not stored in any queue.
+        function : callable or None, optional
+            Function of one argument which will be applied to all items in input_queue. If None (default), 
+            a trivial function that returns the input is used.
+        callback : callable or None, optional
+            Called on each item before being stored in the output queue. Ideal for emitting Qt signals or printing.
+        
+        Raises
+        ------
+        ValueError
+            If callback and output_queue are both None.
+        """
+        if not any((output_queue, callback)):      # output_queue and callback are None
+            raise ValueError('output_queue and callback cannot be both None.')
+        
+        self.input_queue = input_queue if input_queue is not None else ProcessSafeQueue()
+        self.output_queue = output_queue if output_queue is not None else VoidQueue()
+        self.function = function if function is not None else _trivial_function
+        self.callback = callback if callback is not None else _trivial_callback
+        self.worker = None
+    
+    def start(self):
+        """ Start the event loop in a separate thread. """
+        # Start of the reactor is in a separate method to allow control by subclasses.
+        self.worker = Process(target = _func_event_loop, args = (self.input_queue, self.output_queue, self.reaction, self.callback))
+        self.worker.start()
+    
+    def stop(self):
+        """ 
+        Stop the event loop. This method will block until the event loop
+        thread is joined. 
+        """
+        self.worker.terminate()
+        self.worker.join()
+
+def _func_event_loop(input_queue, output_queue, function, callback):
+    """ Functional form of the reactor event loop. """
+    while True:
+        try:
+            item = input_queue.get(timeout = 1)
+        except Empty:
+            pass
+        else:
+            reacted = function(item)
+            callback(item)
+            output_queue.put(reacted)
--- shampoo/gui/tests/test_reactor.py
+++ shampoo/gui/tests/test_reactor.py
@@ -1,9 +1,9 @@
 
-from shampoo.gui.reactor import Reactor, ThreadSafeQueue
+from shampoo.gui.reactor import Reactor, ProcessReactor, ThreadSafeQueue, ProcessSafeQueue
 from time import sleep
 import unittest
 
-class TestReactor(unittest.TestCase):
+class TestThreadReactor(unittest.TestCase):
     
     item = 'foobar'
 
@@ -35,6 +35,41 @@ class TestReactor(unittest.TestCase):
         self.assertFalse(reactor.is_alive())
         reactor.start()
         self.assertTrue(reactor.is_alive())
+
+class TestProcessReactor(TestThreadReactor):
+
+    item = 10
+
+    def setUp(self):
+        self.out_queue = ProcessSafeQueue()
+    
+    def test_trivial_reactor(self):
+        """ 
+        Tests that the trivial reactor -- which takes inputs and places them in the
+        output queue -- works as intended. 
+        """
+        reactor = ProcessReactor(output_queue = self.out_queue, function = None)
+        reactor.start()
+        reactor.send_item(self.item)
+        self.assertEqual(self.out_queue.get(), self.item)
+    
+    def test_reactor_startup(self):
+        """ Tests whether the event loop is running right after start(). """
+        reactor = ProcessReactor(output_queue = self.out_queue, function = None)
+        reactor.start()
+        self.assertTrue(reactor.is_alive())
+    
+    def test_reactor_start_and_stop(self):
+        """ Tests whether the reactor can start, stop, and start again """
+        reactor = ProcessReactor(output_queue = self.out_queue)
+        reactor.start()
+        self.assertTrue(reactor.is_alive())
+        reactor.stop()
+        self.assertFalse(reactor.is_alive())
+        reactor.start()
+        self.assertTrue(reactor.is_alive())
+    
+
 
 if __name__ == '__main__':
     unittest.main()
