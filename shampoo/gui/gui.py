860e565987b3735c08e06dc3e25d1ce0328f87b8
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-04-20 17:00:04 -0400

    Removed real-time reconstruction

--- shampoo/gui/gui.py
+++ shampoo/gui/gui.py
@@ -17,14 +17,13 @@ from skimage.io import imsave
 from . import error_aware
 from ..reconstruction import Hologram, ReconstructedWave
 from ..time_series import TimeSeries
-from .camera import AlliedVisionCamera, available_cameras
 from .controller import ShampooController
-from .debug import DebugCamera
 from .fourier_mask_dialog import FourierMaskDialog
 from .hologram_viewer import HologramViewer
 from .recon_params_widget import ReconstructionParametersWidget
 from .time_series_creator import TimeSeriesCreator
-from .widgets import (CameraFeatureDialog, ReconstructedHologramViewer,
+from .time_series_reconstruction_dialog import TimeSeriesReconstructionDialog
+from .widgets import (ReconstructedHologramViewer,
                       ShampooStatusBar, TimeSeriesControls)
 
 def run(debug = False):
@@ -56,8 +55,6 @@ class App(QtGui.QMainWindow):
         Select the propagation distance(s) with which to reconstruct
         holograms.
     """
-    save_latest_hologram_signal = QtCore.pyqtSignal(object, name = 'save_latest_hologram_signal')
-    connect_camera_signal = QtCore.pyqtSignal(object, name = 'connect_camera_signal')
 
     error_message_signal = QtCore.pyqtSignal(str, name = 'error_message_signal')
     
@@ -70,6 +67,7 @@ class App(QtGui.QMainWindow):
         """
         super(App, self).__init__()
 
+        # TODO: controller in its own thread
         self.controller = ShampooController()
         self.debug = debug
 
@@ -86,7 +84,6 @@ class App(QtGui.QMainWindow):
 
         # Assemble menu from previously-defined actions
         self.file_menu = self.menubar.addMenu('&File')
-        self.camera_menu = self.menubar.addMenu('&Camera')
 
         raw_data_layout = QtGui.QVBoxLayout()
         raw_data_layout.addWidget(self.data_viewer)
@@ -121,20 +118,15 @@ class App(QtGui.QMainWindow):
         self.load_time_series_action.triggered.connect(self.load_time_series)
         self.file_menu.addAction(self.load_time_series_action)
 
-        self.load_data_action = QtGui.QAction('&Load raw data', self)
-        self.load_data_action.triggered.connect(self.load_data)
-        self.file_menu.addAction(self.load_data_action)
-
-        self.save_data_action = QtGui.QAction('&Save raw data', self)
-        self.save_data_action.triggered.connect(self.save_raw_data)
-        self.file_menu.addAction(self.save_data_action)
-        self.save_data_action.setEnabled(False)
-
         self.file_menu.addSeparator()
 
-        self.time_series_creator_action = QtGui.QAction('&Create hologram time series', self)
+        self.time_series_creator_action = QtGui.QAction('&Create hologram times-eries', self)
         self.time_series_creator_action.triggered.connect(self.launch_time_series_creator)
         self.file_menu.addAction(self.time_series_creator_action)
+
+        self.time_series_reconstruct_action = QtGui.QAction('&Reconstruct a hologram time-series', self)
+        self.time_series_reconstruct_action.triggered.connect(self.launch_time_series_reconstruction)
+        self.file_menu.addAction(self.time_series_reconstruct_action)
 
         self.file_menu.addSeparator()
 
@@ -142,37 +134,12 @@ class App(QtGui.QMainWindow):
         self.load_fourier_mask_action.triggered.connect(self.load_fourier_mask)
         self.file_menu.addAction(self.load_fourier_mask_action)
 
-        self.connect_camera_action = QtGui.QAction('&Connect a camera', self)
-        self.connect_camera_action.triggered.connect(self.connect_camera)
-        self.camera_menu.addAction(self.connect_camera_action)
-
-        self.camera_snapshot_action = QtGui.QAction('&Take camera snapshot', self)
-        self.camera_snapshot_action.triggered.connect(self.controller.send_snapshot_data)
-        self.camera_menu.addAction(self.camera_snapshot_action)
-        self.camera_snapshot_action.setEnabled(False)
-
-        self.camera_features_action = QtGui.QAction('&Change camera features', self)
-        self.camera_features_action.triggered.connect(self.change_camera_features)
-        self.camera_menu.addAction(self.camera_features_action)
-        self.camera_features_action.setEnabled(False)
-
         self.reconstruction_parameters_widget.propagation_distance_signal.connect(self.controller.update_propagation_distance)
         self.controller.reconstructed_hologram_signal.connect(self.reconstructed_viewer.display)
         self.controller.raw_data_signal.connect(self.data_viewer.display)
 
-        # Save and loads
-        self.save_latest_hologram_signal.connect(self.controller.save_latest_hologram)
-
         # Controller status signals
-        self.connect_camera_signal.connect(self.controller.connect_camera)
         self.controller.reconstruction_status_signal.connect(self.status_bar.update_status)
-
-        # What actions are available when a camera is made available
-        # These actions will become unavailable when a camera is disconnected.
-        self.controller.camera_connected_signal.connect(lambda x: self.status_bar.update_status('Camera connected'))
-        self.controller.camera_connected_signal.connect(self.camera_snapshot_action.setEnabled)
-        self.controller.camera_connected_signal.connect(self.camera_features_action.setEnabled)
-        self.controller.raw_data_signal.connect(lambda x: self.save_data_action.setEnabled(True))
 
         self.reconstruction_parameters_widget.update_propagation_distance()
 
@@ -198,12 +165,19 @@ class App(QtGui.QMainWindow):
         path = self.file_dialog.getOpenFileName(self, 'Load time-series', filter = '*hdf5 *.h5')[0]
         self.controller.load_time_series(path)
     
-    @error_aware('The hologram time series could not be created.')
+    @error_aware('The hologram time-series could not be created.')
     @QtCore.pyqtSlot()
     def launch_time_series_creator(self):
         time_series_creator = TimeSeriesCreator(parent = self)
         time_series_creator.time_series_path_signal.connect(self.controller.load_time_series)
         success = time_series_creator.exec_()
+    
+    @error_aware('The hologram time-series could not be reconstructed.')
+    @QtCore.pyqtSlot()
+    def launch_time_series_reconstruction(self):
+        time_series_reconstruction = TimeSeriesReconstructionDialog(parent = self)
+        time_series_reconstruction.time_series_reconstructed.connect(self.controller.load_time_series)
+        success = time_series_reconstruction.exec_()
     
     @error_aware('Raw data could not be saved.')
     @QtCore.pyqtSlot()
@@ -213,34 +187,6 @@ class App(QtGui.QMainWindow):
         if not path.endswith('.tif'):
             path = path + '.tif'
         self.save_latest_hologram_signal.emit(path)
-    
-    @error_aware('Camera could not be connected')
-    @QtCore.pyqtSlot()
-    def connect_camera(self):
-        """ Bring up a modal dialog to choose amongst available cameras. """
-        cameras = available_cameras()
-
-        if self.debug:
-            cameras.append('debug')
-        
-        if not cameras:
-            self.error_message_signal.emit('No cameras available.')
-            return
-        
-        camera_ID, ok = QtGui.QInputDialog.getItem(self, 'Select camera', 'List of cameras', 
-                                                   cameras, 0, False)
-        
-        if ok and camera_ID:
-            self.connect_camera_signal.emit(camera_ID)
-    
-    @error_aware('Camera features could not be updated.')
-    @QtCore.pyqtSlot()
-    def change_camera_features(self):
-        self.camera_features_dialog = CameraFeatureDialog(camera = self.controller.camera, parent = self)
-        self.camera_features_dialog.camera_features_update_signal.connect(self.controller.update_camera_features)
-        success = self.camera_features_dialog.exec_()
-        if not success:
-            raise RuntimeError
     
     def closeEvent(self, event):
         reply = QtGui.QMessageBox.question(self, 'SHAMPOO', 'Are you sure you want to quit?', 
